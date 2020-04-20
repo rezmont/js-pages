54aeb53343ef0aa5514e65503c33137bb75eea1e
rezmont <reza.motamedi@gmail.com>
2020-04-19 18:10:31 -0700

    Updates

---
+++ precache-manifest.6ba003963ebe3e736ceb3468dd041ff1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dcd2649f584dc4051c9dc1f5a08f80f",
+    "url": "/rezmont/js-pages/index.html"
+  },
+  {
+    "revision": "a6a13956080e23b971e0",
+    "url": "/rezmont/js-pages/static/css/main.770667c9.chunk.css"
+  },
+  {
+    "revision": "225ac8c210102142c6ac",
+    "url": "/rezmont/js-pages/static/js/2.738bf4e9.chunk.js"
+  },
+  {
+    "revision": "c3f4a91b08326551c3d2c23a3296dfa8",
+    "url": "/rezmont/js-pages/static/js/2.738bf4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6a13956080e23b971e0",
+    "url": "/rezmont/js-pages/static/js/main.87539f99.chunk.js"
+  },
+  {
+    "revision": "b7f756cb2a0d61abde8e",
+    "url": "/rezmont/js-pages/static/js/runtime-main.1b02c363.js"
+  }
+]);
